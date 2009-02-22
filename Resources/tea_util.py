74e419ac3882a004dfdcd88be5063f72b1ca5565
Ian Beck <ian@onecrayon.com>
2009-02-22 14:12:19 -0800

    Added several shortcut classes to utils

--- Resources/tea_util.py
+++ Resources/tea_util.py
@@ -3,9 +3,14 @@
 from Foundation import *
 import objc
 
+from espresso import *
+
+# ===============================================================
+# Interact with the user
+# ===============================================================
 
 def ask(context, question, default=''):
-    '''Displays a dialog box asking for user input (textfield)'''
+    '''Displays a dialog box asking for user input'''
     return True
 
 def say(context, title, message,
@@ -22,3 +27,27 @@ def say(context, title, message,
     return alert.beginSheetModalForWindow_modalDelegate_didEndSelector_contextInfo_(
         context.windowForSheet(), None, None, None
     )
+
+# ===============================================================
+# Interact with the text window
+# ===============================================================
+
+def get_range_set(context):
+    '''
+    Returns the MRRangeSet for the selection in the current context
+    
+    For range set methods, see Espresso.app/Contents/Headers/MRRangeSet.h
+    '''
+    return MRRangeSet.alloc().initWithRangeValues_(context.selectedRanges())
+
+def new_recipe():
+    '''
+    Shortcut to create a new text recipe
+    
+    For recipe methods, see Espresso.app/Contents/Headers/EspressoTextCore.h
+    '''
+    return CETextRecipe.textRecipe()
+
+def snippet(snippet):
+    '''Shortcut to create a new text snippet'''
+    return CETextSnippet.snippetWithString_(snippet)
