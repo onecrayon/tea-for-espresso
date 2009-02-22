d8ab39cfa20d15f46c9c9c6531b7df86c3a5f4c2
Ian Beck <ian@onecrayon.com>
2009-02-22 11:28:12 -0800

    Added automatic directory walking to populate included directories

--- setup.py
+++ setup.py
@@ -5,14 +5,38 @@
 
 from distutils.core import setup
 import py2app
+import os
+
+
+def include_files(path):
+    '''
+    Walks a given folder and returns a list of its contents
+    
+    List includes the folder's name (so that we can use it with data_files)
+    '''
+    files = []
+    for root, dirs, filenames in os.walk(path):
+        list_fragment = []
+        if filenames:
+            for file in filenames:
+                # Don't include hidden files
+                if (file[0] != '.'):
+                    files.append(os.path.join(root, file))
+    print(files)
+    return files
+
+
+# Configure this by hand for any included directories
+includes = [
+    ('../../TextActions', include_files('./TextActions')),
+    ('../../TEA', include_files('./TEA')),
+    ('./', include_files('./Resources')),
+]
 
 setup(
     name='TEA for Espresso',
     plugin = ['TEAforEspresso.py'],
-    data_files = [('../../TextActions', ['TextActions/Actions.xml',
-                   'TextActions/Categories.xml']),
-                  ('../../TEA', ['TEA/wrap_selection_in_tag.py'])
-                 ],
+    data_files = includes,
     options=dict(py2app=dict(
         extension='.sugar',
         semi_standalone = True,
