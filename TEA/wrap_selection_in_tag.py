21e6e3c5bae25a0a1c08efa89d509863305d598d
Ian Beck <ian@onecrayon.com>
2009-02-22 14:12:37 -0800

    Testing snippet insertion

--- TEA/wrap_selection_in_tag.py
+++ TEA/wrap_selection_in_tag.py
@@ -8,6 +8,12 @@ Wraps the currently selected text in a tag supplied by the user
 import tea_util as tea
 
 def act(context):
-    response = tea.say(context, 'Success!',
-                       'Successfully ran Wrap Selection as Tag')
+    '''Required method; performs the action'''
+    context.insertTextSnippet_(tea.snippet('<#{1:p}>SELECTED_TEXT</#{1/\s.*//}>#0'))
+#     ranges = context.selectedRanges()
+#     for range in ranges:
+#         print(range)
+    
+#     response = tea.say(context, 'Success!',
+#                        'Successfully ran Wrap Selection as Tag')
     return True
