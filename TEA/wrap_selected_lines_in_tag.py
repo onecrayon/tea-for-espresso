6f641ce81a0743f6f8ca9ed667fedf7f3e9d664e
Ian Beck <ian@onecrayon.com>
2009-02-23 22:04:54 -0800

    Added docstring comments describing action

--- TEA/wrap_selected_lines_in_tag.py
+++ TEA/wrap_selected_lines_in_tag.py
@@ -5,5 +5,11 @@ Wraps currently selected lines in a text snippet
 import tea_utils as tea
 
 def act(context):
-    '''Required action method'''
+    '''
+    Required action method
     
+    Much like Wrap Selection in Tag, this only allows a single selection
+    (enforced through the utility functions) then parses over the
+    lines and inserts a snippet to allow the user to define the tag
+    '''
+    # TODO: actions go here
