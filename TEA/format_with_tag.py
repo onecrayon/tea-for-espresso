7948b62b25e52692541fef2e71480971dab4efd1
Ian Beck <ian@onecrayon.com>
2009-02-25 08:09:08 -0800

    Modified format with tag to handle discontiguous selections

--- TEA/format_with_tag.py
+++ TEA/format_with_tag.py
@@ -16,13 +16,27 @@ def act(context, tag='p', tagname=None):
     sensible default.  See TextActions/Actions.xml for an example of
     how to construct those arguments in XML definition.
     '''
-    text, range = tea.get_single_selection(context)
-    if text == None:
-        return False
-    snippet = '#{1:<' + tag + '>#{2:' + text + '}</' + tag + '>}#0'
     # Set the legible tag name
     if tagname == None:
         tagname = tag.capitalize()
-    # Insert the text via recipe
-    return tea.insert_snippet_over_selection(context, snippet, range,
-                                             'Format with ' + tagname)
+    ranges = context.selectedRanges()
+    if len(ranges) <= 1:
+        # If we're working with a single selection, we can use a snippet
+        text, range = tea.get_single_selection(context)
+        if text == None:
+            return False
+        snippet = '#{1:<' + tag + '>#{2:' + text + '}</' + tag + '>}#0'
+        # Insert the text via recipe
+        return tea.insert_snippet_over_selection(context, snippet, range,
+                                                 'Format with ' + tagname)
+    # We're handling multiple, discontiguous ranges; wrap all of them
+    # with the tag
+    insertions = tea.new_recipe()
+    for range in ranges:
+        # Convert NSConcreteValue to NSRange
+        range = range.rangeValue()
+        text = tea.get_selection(context, range)
+        text = '<' + tag + '>' + text + '</' + tag + '>'
+        insertions.addReplacementString_forRange_(text, range)
+    insertions.setUndoActionName_('Format with ' + tagname)
+    return context.applyTextRecipe_(insertions)
