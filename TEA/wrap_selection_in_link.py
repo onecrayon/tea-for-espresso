8533b06baf1aff93d41127bb333a2b3dbd4931c6
Ian Beck <ian@onecrayon.com>
2009-02-25 22:54:00 -0800

    Added skeleton for Wrap Selection in Link

---
+++ TEA/wrap_selection_in_link.py
@@ -0,0 +1,13 @@
+'''Wraps selected text in a link (what kind of link based on context)'''
+
+import tea_actions as tea
+
+def act(context):
+    '''
+    Required action method
+    
+    A flexible link generator which uses the clipboard text (if there's
+    a recognizable link there) and formats the snippet based on the
+    active syntax of the context
+    '''
+    pass
