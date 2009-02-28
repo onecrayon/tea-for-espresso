b8d29f4c959c00c42b02d102177f0d5b778a8d2e
Ian Beck <ian@onecrayon.com>
2009-02-28 08:16:28 -0800

    Migrated 'get word' internal logic into tea_actions

--- TEA/insert_tag_from_word.py
+++ TEA/insert_tag_from_word.py
@@ -1,7 +1,5 @@
 '''Converts word or selection into an open/close tag pair'''
 
-from Foundation import NSMakeRange, NSArray, NSValue
-
 import tea_actions as tea
 
 def act(context):
@@ -19,62 +17,12 @@ def act(context):
     range = tea.get_single_range(context)
     if range == None:
         return False
-    if range.length == 0:
-        # Set up basic variables
-        index = range.location
-        tag = ''
-        maxlength = context.string().length()
-        # Make sure the cursor isn't at the end of the document
-        if index != maxlength:
-            # Check if cursor is mid-word
-            char = tea.get_selection(context, NSMakeRange(index, 1))
-            if char.isalpha():
-                inword = True
-                # Parse forward until we hit the end of word or document
-                while inword:
-                    char = tea.get_selection(context, NSMakeRange(index, 1))
-                    if char.isalpha():
-                        tag += char
-                    else:
-                        inword = False
-                    index += 1
-                    if index == maxlength:
-                        inword = False
-            else:
-                # lastindex logic assumes we've been incrementing as we go,
-                # so bump it up one to compensate
-                index += 1
-        lastindex = index - 1 if index < maxlength else index
-        # Reset index to one less than the cursor
-        index = range.location - 1
-        # Only walk backwards if we aren't at the beginning
-        if index > 0:
-            # Parse backward to get the word ahead of the cursor
-            inword = True
-            while inword:
-                char = tea.get_selection(context, NSMakeRange(index, 1))
-                if char.isalpha():
-                    tag = char + tag
-                else:
-                    inword = False
-                index -= 1
-                if index < 0:
-                    inword = False
-        # Since index is left-aligned and we've overcompensated,
-        # need to increment +2
-        firstindex = index + 2 if index > 0 else 0
-        # Switch last index to length for use in range
-        lastindex = lastindex - firstindex
-        range = NSMakeRange(firstindex, lastindex)
-        # setSelectedRanges requires an array of ranges, which actually means
-        # an array of NSValue range objects
-        ranges = NSArray.arrayWithObjects_(NSValue.valueWithRange_(range))
-        context.setSelectedRanges_(ranges)
-        opentag = closetag = tag
-    else:
-        tag = tea.get_selection(context, range)
-        # Parse the tag since there's almost certainly attributes
+    tag = tea.get_word_or_selection(context, range)
+    if ' \t\n\r' in tag:
+        # There's a space character, so parse it as a tag
         opentag, closetag = tea.parse_tag(tag)
+    else:
+        opentag = closetag = tag
     # Construct the snippet and insert it
     snippet = '<' + opentag + '>$1</' + closetag + '>$0'
     return tea.insert_snippet_over_selection(context, snippet, range,
