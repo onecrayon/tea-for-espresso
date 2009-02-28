0dbc3916d3f200c760f3f3ef7990c7a297177a3e
Ian Beck <ian@onecrayon.com>
2009-02-27 18:43:38 -0800

    Vastly simplified the code (only using single selection, no extra functions)

--- TEA/wrap_selection_in_link.py
+++ TEA/wrap_selection_in_link.py
@@ -4,21 +4,9 @@ import subprocess
 
 import tea_actions as tea
 
-def create_link(zone, url, text, snippet=False):
-    '''
-    Creates a textual link (or snippet) based on the active syntax zone
-    (defaults to HTML)
-    '''
-    if snippet:
-        return tea.construct_snippet(
-            text,
-            '<a href="${1:' + url + '}"#{2: title="#3"}>$SELECTED_TEXT</a>$0'
-        )
-    # Not a snippet, so return the text
-    return '<a href="' + url + '">' + text + '</a>'
-
-
-def act(context, undo_name='Wrap Selection In Link'):
+def act(context, undo_name='Wrap Selection In Link',
+        default='<a href="${1:$URL}"#{2: title="#3"}>$SELECTED_TEXT</a>$0',
+        **syntaxes):
     '''
     Required action method
     
@@ -32,32 +20,14 @@ def act(context, undo_name='Wrap Selection In Link'):
     # Construct the default link
     url = clipboard
     
-    ranges = tea.get_ranges(context)
-    if len(ranges) == 1:
-        # If we're working with a single selection, we can use a snippet
-        range = ranges[0]
-        # Make sure the range is actually a selection
-        if range.length == 0:
-            tea.say(
-                context, "Error: selection required",
-                "You must select some text in order to use this action."
-            )
-            return False
-        text = tea.get_selection(context, range)
-        # Get the syntax zone
-        zone = ''
-        snippet = create_link(zone, url, text, snippet=True)
-        # Insert the text via recipe
-        return tea.insert_snippet_over_selection(context, snippet, range,
-                                                 undo_name)
-    # We're handling multiple, discontiguous ranges; wrap all of them
-    # with the tag
-    insertions = tea.new_recipe()
-    for range in ranges:
-        text = tea.get_selection(context, range)
-        # Get the syntax zone
-        zone = ''
-        text = create_link(zone, url, text)
-        insertions.addReplacementString_forRange_(text, range)
-    insertions.setUndoActionName_(undo_name)
-    return context.applyTextRecipe_(insertions)
+    text, range = tea.get_single_selection(context)
+    if text == None:
+        return False
+    # Get the syntax zone
+    zone = ''
+    # Get the snippet based on the zone
+    snippet = default
+    snippet = tea.construct_snippet(text, snippet)
+    snippet.replace('$URL', url)
+    return tea.insert_snippet_over_selection(context, snippet, range,
+                                             undo_name)
