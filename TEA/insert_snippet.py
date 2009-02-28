fddce9f7f6ae8649d2fa24be9057f61ed106f459
Ian Beck <ian@onecrayon.com>
2009-02-28 14:49:16 -0800

    Added basic logic for snippet insertion

--- TEA/insert_snippet.py
+++ TEA/insert_snippet.py
@@ -2,11 +2,28 @@
 
 import tea_actions as tea
 
-def act(context, snippet=None, **syntaxes):
+def act(context, default=None, **syntaxes):
     '''
     Required action method
     
     Inserts an arbitrary text snippet after the cursor with provisions for
     syntax-specific alternatives
+    
+    This method requires at least the snippet default to be defined in the XML
     '''
-    pass
+    if default is None:
+        return False
+    # Get the cursor position
+    range = tea.get_single_range(context)
+    # Check for root-zone specific override
+    root_zone = tea.get_root_zone(context)
+    if root_zone in syntaxes:
+        snippet = syntaxes[root_zone]
+    else:
+        snippet = default
+    # Check for specific zone override
+    zone = tea.get_active_zone(context, range)
+    if zone in syntaxes:
+        snippet = syntaxes[zone]
+    # Insert that snippet!
+    return tea.insert_snippet(context, snippet)
