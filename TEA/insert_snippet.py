4f6fd6654260ce7bf177d4992a6d879205f142ac
Ian Beck <ian@onecrayon.com>
2009-02-28 14:43:43 -0800

    Added skeleton for Insert Snippet

---
+++ TEA/insert_snippet.py
@@ -0,0 +1,12 @@
+'''Inserts a snippet at the user's cursor; useful for tab completions'''
+
+import tea_actions as tea
+
+def act(context, snippet=None, **syntaxes):
+    '''
+    Required action method
+    
+    Inserts an arbitrary text snippet after the cursor with provisions for
+    syntax-specific alternatives
+    '''
+    pass
