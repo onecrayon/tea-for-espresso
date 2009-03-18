3566f064273d3e3426c57c2cd2f70e9444248681
Ian Beck <ian@onecrayon.com>
2009-03-17 19:32:41 -0700

    Added menu options for convert to entities actions

---
+++ src/TEA/convert_to_entities.py
@@ -0,0 +1,14 @@
+'''
+Converts characters in selected (or the nearest character to the cursor)
+into HTML entities
+'''
+
+import tea_actions as tea
+
+def act(context, type='both'):
+    '''
+    Required action method
+    
+    Will convert into either named entities, numeric entities, or both
+    '''
+    pass
