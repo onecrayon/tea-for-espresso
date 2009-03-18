269f1511c83f0884b75d7a9bda6c5fb56412e327
Ian Beck <ian@onecrayon.com>
2009-03-17 22:42:02 -0700

    Added automatic conversion for character next to cursor if no selection

--- src/TEA/convert_to_entities.py
+++ src/TEA/convert_to_entities.py
@@ -11,8 +11,20 @@ def act(context, type='named', ampersands='named', undo_name=None):
     
     Will convert into either named entities (with any high-value, non-named
     entities as numeric entities) or numeric entities
+    
+    The ampersands parameter applies only to numeric entities (sets whether
+    to use named or numeric ampersands)
     '''
     ranges = tea.get_ranges(context)
+    if len(ranges) is 1 and ranges[0].length is 0:
+        # We've got one empty range; make sure it's not at the
+        # beginning of the document
+        if ranges[0].location > 0:
+            # Set the new target range to the character before the cursor
+            ranges[0] = tea.new_range(ranges[0].location - 1, 1)
+        else:
+            return False
+    # Since we're here we've got something to work with
     insertions = tea.new_recipe()
     for range in ranges:
         text = tea.get_selection(context, range)
