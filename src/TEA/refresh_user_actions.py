594c4af9a7098040dcec584617491ac0c83d04e9
Ian Beck <ian@onecrayon.com>
2009-03-27 08:15:28 -0700

    Added basic skeleton for symlink refreshing actions

---
+++ src/TEA/refresh_user_actions.py
@@ -0,0 +1,10 @@
+'''
+Refreshes symlinks to user's Application Support directory to enable
+custom user TEA actions
+'''
+
+import tea_actions as tea
+
+def act(context, notify=True):
+    '''If notify=True, will tell the user once the symlinks are generated'''
+    pass
