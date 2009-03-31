606be4899b1f0a2a0bccd4087739c782225df0ec
Ian Beck <ian@onecrayon.com>
2009-03-30 19:03:33 -0700

    Added defaults swapping and custom messages

--- src/TEA/refresh_user_actions.py
+++ src/TEA/refresh_user_actions.py
@@ -15,20 +15,26 @@ def act(context, notify=True):
     If notify=True, will tell the user once the symlinks are generated
     '''
     defaults = NSUserDefaults.standardUserDefaults()
-    enabled = defaults.stringForKey_('TEAEnableUserActions')
-    if enabled is True:
+    enabled = defaults.boolForKey_('TEAEnableUserActions')
+    if enabled:
         # switch preference to false
-        pass
+        defaults.setBool_forKey_(False, 'TEAEnableUserActions')
+        title = 'Custom User Actions Disabled'
+        text = 'You have successfully disabled custom user actions. Relaunch '
+               'Espresso to remove your actions from the menus.'
     else:
         # switch preference to true, or add it if it doesn't exist
-        pass
-    
-    refresh_symlinks()
+        defaults.setBool_forKey_(True, 'TEAEnableUserActions')
+        title = 'Custom User Actions Enabled'
+        text = 'You have successfully enabled custom user actions. '
+               'You must relaunch Espresso in order to load your custom '
+               'actions.\n\nChanges to your custom actions will be refreshed '
+               'whenever you relaunch Espresso. If you add files to your custom '
+               'action folder, you will need to relaunch Espresso twice for '
+               'them to take effect.'
+    # Add or remove the symlinks
+    refresh_symlinks(True)
     
     if notify:
-        tea.say(
-            context, 'Custom User Actions Enabled',
-            'You have successfully enabled custom user actions. '
-            'Please relaunch Espresso in order to load your actions.'
-        )
+        tea.say(context, title, text)
     return True
