dc947cd6bb5c7a4afe68b4ea50f84f308906e703
Ian Beck <ian@onecrayon.com>
2009-04-04 21:02:18 -0700

    Set up script loading with basic commented example code

--- src/Contents/Resources/TEALoader.py
+++ src/Contents/Resources/TEALoader.py
@@ -4,6 +4,9 @@ Text Editor Actions Loader
 A loader class to execute arbitrary shell scripts within Espresso
 '''
 
+import subprocess
+import os
+
 from Foundation import *
 import objc
 
@@ -25,12 +28,16 @@ class TEALoader(NSObject):
         if self is None: return None
         
         # Set object's internal variables
+        self.script = dictionary['script'] if 'script' in dictionary else None
+        self.input = dictionary['input'] if 'input' in dictionary else None
+        self.alternative = dictionary['alternative'] \
+                           if 'alternative' in dictionary else None
+        self.output = dictionary['output'] if 'output' in dictionary else None
+        self.undo = dictionary['undo_name'] if 'undo_name' in dictionary else None
         
         # Set the syntax context
-        if 'syntax-context' in dictionary:
-            self.syntax_context = dictionary['syntax-context']
-        else:
-            self.syntax_context = None
+        self.syntax_context = dictionary['syntax-context'] \
+                              if 'syntax-context' in dictionary else None
         
         # By looking up the bundle, third party sugars can call
         # TEA for Espresso actions or include their own custom actions
@@ -46,7 +53,7 @@ class TEALoader(NSObject):
             ranges = context.selectedRanges()
             range = ranges[0].rangeValue()
             selectors = SXSelectorGroup.selectorGroupWithString_(self.syntax_context)
-            zone = context.syntaxTree().root().zoneAtCharacterIndex_(range.location);
+            zone = context.syntaxTree().root().zoneAtCharacterIndex_(range.location)
             if selectors.matches_(zone):
                 return True
             else:
@@ -59,4 +66,39 @@ class TEALoader(NSObject):
         Gathers the necessary info, populates the environment, and runs
         the script
         '''
-        pass
+        def execute(file, input):
+            '''Utility function for running the script'''
+            script = subprocess.Popen(
+                [file],
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE
+            )
+            return script.communicate(input)
+        
+        if self.script is None:
+            return False
+        # Set up the environment
+        # USE: os.putenv('VARNAME', 'var value')
+        
+        # Get the requested info
+        # EXAMPLE: input, range = tea.get_single_selection(context)
+        
+        # Run the script
+        file = os.path.join(self.bundle_path, 'TEA', self.script)
+        try:
+            output, error = execute(file, input)
+        except:
+            # Most likely cause of failure is lack of executable status on file
+            try:
+                os.chmod(file, 0755)
+                output, error = execute(file, input)
+            except:
+                # Likely failure is inability to set executable status, so exit
+                return tea.say(
+                    context, 'Error: cannot execute script',
+                    'Error: could not execute the script. Please contact the '\
+                    'Sugar author.'
+                )
+        # Process the output
+        return True
