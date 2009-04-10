edd1a7be60e5dcd05bc4f833c89af3d16121cbc8
Ian Beck <ian@onecrayon.com>
2009-04-09 19:58:19 -0700

    Added skeleton for spaces_to_tabs action

---
+++ src/TEA/spaces_to_tabs.py
@@ -0,0 +1,14 @@
+'''
+Converts spaces to tabs and vice versa
+'''
+
+import tea_actions as tea
+
+def act(context, convert_to=None, undo_name=None):
+    '''
+    Required action method
+    
+    convert_to can either be 'tabs' or 'spaces'; by default, it will use
+    the text preferences
+    '''
+    pass
