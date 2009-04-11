43984ac37b104a500213fb387fa83199b6cf0932
Ian Beck <ian@onecrayon.com>
2009-04-11 13:46:21 -0700

    Finalized visit_url; added example action for HTML to lookup tags online [#14 state:resolved]

--- src/TEA/visit_url.py
+++ src/TEA/visit_url.py
@@ -2,6 +2,9 @@
 Visits a URL, filling a placeholder with selected text (or similar)
 '''
 
+import urllib
+import webbrowser
+
 import tea_actions as tea
 
 def act(context, input=None, default=None, **syntaxes):
@@ -15,4 +18,30 @@ def act(context, input=None, default=None, **syntaxes):
     default and syntaxes will replace $SELECTED_TEXT with a URL escaped version
     of the selected text (or input, if no selected text)
     '''
-    pass
+    text, range = tea.get_single_selection(context)
+    if text is None:
+        range = tea.get_single_range(context)
+        if input == 'word':
+            text, range = tea.get_word(context, range)
+        elif input == 'line':
+            text, range = tea.get_line(context, range)
+    # If we still don't have text, there's nothing to work with
+    if text is None:
+        return False
+    # URL escape the selected text
+    text = urllib.quote_plus(text)
+    root_zone = tea.get_root_zone(context)
+    if root_zone in syntaxes:
+        root_url = syntaxes[root_zone]
+    else:
+        root_url = default
+    zone = tea.get_active_zone(context, range)
+    if zone in syntaxes:
+        url = syntaxes[zone]
+    else:
+        url = root_url
+    # Got the URL, let's run the URL
+    url = url.replace('$SELECTED_TEXT', text)
+    webbrowser.open(url)
+    # Returning false prevents Espresso from beeping
+    return False
