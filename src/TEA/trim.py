ece87c0741318f07f8809ebfedfe5ed45c3fee4e
Ian Beck <ian@onecrayon.com>
2009-04-11 12:20:16 -0700

    Added logic for trimming input vs. alternative

--- src/TEA/trim.py
+++ src/TEA/trim.py
@@ -4,7 +4,8 @@ Trims the text; what is trimmed depends on what's passed in via XML
 
 import tea_actions as tea
 
-def act(context, input=None, alternate=None, trim='both', respect_indent=False):
+def act(context, input=None, alternate=None, trim='both', respect_indent=False,
+        undo_name=None):
     '''
     Required action method
     
@@ -26,4 +27,27 @@ def act(context, input=None, alternate=None, trim='both', respect_indent=False):
     If respect_indent is True, indent characters (as defined in preferences)
     at the beginning of the line will be left untouched.
     '''
-    pass
+    # Since input is always a selection of some kind, check if we have one
+    ranges = tea.get_ranges(context)
+    insertions = tea.new_recipe()
+    if (len(ranges) == 1 and ranges[0].length = 0) or input is None:
+        if alternate == 'line':
+            text, range = tea.get_line(context, ranges[0])
+            text = tea.trim(context, text, False, trim, respect_indent)
+        elif alternate == 'all_lines':
+            range = tea.new_range(0, context.string().length())
+            text = tea.get_selection(context, range)
+            text = tea.trim(context, text, True, trim, respect_indent)
+        insertions.addReplacementString_forRange_(text, range)
+    else:
+        if input == 'selected_lines':
+            parse_lines = True
+        else:
+            parse_lines = False
+        for range in ranges:
+            text = tea.get_selection(context, range)
+            text = tea.trim(context, text, parse_lines, trim, respect_indent)
+            insertions.addReplacementString_forRange_(text, range)
+    if undo_name != None:
+        insertions.setUndoActionName_(undo_name)
+    return context.applyTextRecipe_(insertions)
