73b358b4cc796f5c0a858bef319b531579608721
Ian Beck <ian@onecrayon.com>
2009-04-16 08:09:48 -0700

    1.0b10 for testing

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ import os
 
 # Update this info by hand; defines the required Info.plist elements
 info = dict(
-    CFBundleVersion = '1.0b9a',
+    CFBundleVersion = '1.0b10',
     CFBundleIdentifier = 'com.onecrayon.tea.espresso',
     NSHumanReadableCopyright = '(c) 2009 Ian Beck under the MIT license',
 )
