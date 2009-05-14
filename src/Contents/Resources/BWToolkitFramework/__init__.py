67d56c0c1c01305a6fd5a4179e41184785833967
Ian Beck <ian@onecrayon.com>
2009-05-13 20:16:47 -0700

    Testing action and BWToolkitFramework loader

--- src/Contents/Resources/BWToolkitFramework/__init__.py
+++ src/Contents/Resources/BWToolkitFramework/__init__.py
@@ -1,24 +1,20 @@
 '''
 Python mapping for the BWToolkit framework.
-
-Many thanks to Johan Rydberg
-<http://github.com/jrydberg/pyobjc-bwtoolkitframework/>
 '''
 
 import os.path
 
-from Framework import NSBundle
+from Foundation import NSBundle
 import objc
- 
-__bundle__ = objc.initFrameworkWrapper("BWToolkitFramework",
-    frameworkIdentifier="com.brandonwalkin.BWToolkitFramework",
-    frameworkPath=objc.pathForFramework(
-		os.path.join(
-	        NSBundle.bundleWithIdentifier_('com.onecrayon.tea.espresso').bundlePath(),
-			"Contents",
-			"Frameworks", 
-			"BWToolkitFramework.framework"
-		)
-	),
-    globals=globals()
+
+objc.loadBundle("BWToolkitFramework",
+	globals(),
+    bundle_path=objc.pathForFramework(
+        os.path.join(
+            NSBundle.bundleWithIdentifier_('com.onecrayon.tea.espresso').bundlePath(),
+            "Contents",
+            "Frameworks", 
+            "BWToolkitFramework.framework"
+        )
+    )
 )
