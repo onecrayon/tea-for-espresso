5d921e35f9474a527a8f6f6f156376e45f77b536
Ian Beck <ian@onecrayon.com>
2009-05-30 09:56:25 -0700

    Basic preferences window up and running

--- src/Contents/Resources/TEAPreferences.py
+++ src/Contents/Resources/TEAPreferences.py
@@ -11,8 +11,6 @@ import BWToolkitFramework
 
 class TEAPreferences(TEAforEspresso):
     '''Controller for TEA preference window'''
-    customSheet = objc.IBOutlet()
-    example = objc.IBOutlet()
     
     @objc.signature('B@:@')
     def performActionWithContext_error_(self, context):
@@ -21,27 +19,12 @@ class TEAPreferences(TEAforEspresso):
         preferences
         '''
         # Load the sheet
-        if not self.customSheet:
-            NSBundle.loadNibNamed_owner_('TEAPreferences', self)
-        NSApp.beginSheet_modalForWindow_modalDelegate_didEndSelector_contextInfo_(
-            self.customSheet,
-            context.windowForSheet(),
-            self,
-            'didEndSheet:returnCode:contextInfo:',
-            None
+        prefs = TEAPreferencesController.alloc().initWithWindowNibName_(
+        	'TEAPreferences'
         )
-        # Save the context for later reference (once the sheet is complete)
-        self.context = context
+        prefs.showWindow_(self)
+        prefs.window().center()
         return True
-    
-    @objc.IBAction
-    def doSubmitSheet_(self, sender):
-        NSApp.endSheet_returnCode_(self.customSheet, 1)
-    
-    @objc.IBAction
-    def cancel_(self, sender):
-        NSApp.endSheet_returnCode_(self.customSheet, 0)
-    
-    @AppHelper.endSheetMethod
-    def didEndSheet_returnCode_contextInfo_(self, sheet, code, info):
-        sheet.orderOut_(self)
+
+class TEAPreferencesController(NSWindowController):
+	somevar = objc.IBOutlet()
