e059658a8bb83db8b517084c5b897575f2f504c3
Ian Beck <ian@onecrayon.com>
2009-06-23 18:32:18 -0700

    Added further description of goto's functionality

--- src/TEA/goto.py
+++ src/TEA/goto.py
@@ -4,24 +4,31 @@ Attempts to go to (select) a location in the document
 
 import tea_actions as tea
 
-def act(context, target=None, source=None, prompt=None, search_string=None,
-        regex=False):
+def act(context, target=None, source=None, trim=False, discard_indent=False,
+        prompt=None, search_string=None, regex=False):
     '''
     Required action method
     
     target dictates what we're looking for:
     - text
-    - zone (by syntax zone selector string)
+    - zone (syntax zone selector string)
+    - if unspecified, simply selects the source
     
     source dictates how to gather the string to search for:
     - user (user will be prompted to enter a string)
     - selection
     - word (word under the caret)
+    - line (line under the caret)
+    
+    Setting trim=True will cause the source to be trimmed
+    
+    Setting discard_indent=True will cause even indentation characters
+    to be trimmed (unnecessary unless trim=True)
     
     prompt will set the text of the dialog box if source='user'
     
-    search_string will set the string to search for if target is text or zone;
-    $SELECTED_TEXT will be replaced with the source text
+    search_string will set the string to search for if target is text or zone
+    - $SELECTED_TEXT will be replaced with the source text
     
     Setting regex=True will cause search_string to be evaluated as regex
     '''
