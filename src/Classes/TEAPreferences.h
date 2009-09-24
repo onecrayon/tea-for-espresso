59143b01168758f96bc1b62933dcddb97b689736
Ian Beck <ian@onecrayon.com>
2009-09-24 08:50:58 -0700

    Added and tested TEAPreferences class; converted to XIB format for IB files

---
+++ src/Classes/TEAPreferences.h
@@ -0,0 +1,50 @@
+//
+//  TEAPreferences.h
+//  TEA for Espresso.sugar
+//
+//  Created by Ian Beck
+//  http://onecrayon.com/tea/
+//
+//  MIT License
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TEAPreferences : NSObject {
+	NSWindowController *prefs;
+}
+
+@property (readonly,retain) NSWindowController *prefs;
+
+@end
+
+// ==========================================================
+
+// The window controller
+@interface TEAPreferencesController : NSWindowController {
+	NSArrayController *arrayController;
+    NSTableView *tableView;
+}
+
+@property (readonly,retain) IBOutlet NSArrayController *arrayController;
+@property (readonly,retain) IBOutlet NSTableView *tableView;
+
+@end
+
+// ==========================================================
+
+// The class that allows us to change nil to an empty string
+
+@interface TEANilToString : NSValueTransformer {
+	
+}
+
++ (Class)transformedValueClass;
++ (BOOL)allowsReverseTransformation;
+
+- (NSString *)transformedValue:(id)value;
+- (NSString *)reverseTransformedValue:(id)value;
+
+@end
+
